78d595b94b9c6176f91c8b8a7db8a7a0eccfdd4c
abmsoft <marwane.abbi@gmail.com>
2019-07-16 16:54:10 +0200

    fix paquage root api

--- src/main/java/abm/com/sol/configuration/SwaggerConfig.java
+++ src/main/java/abm/com/sol/configuration/SwaggerConfig.java
@@ -23,7 +23,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("hsbc.com.sol"))
+                .apis(RequestHandlerSelectors.basePackage("abm.com.sol"))
                 .paths(PathSelectors.any())
                 .build().apiInfo(metaData())
                 .useDefaultResponseMessages(false);
